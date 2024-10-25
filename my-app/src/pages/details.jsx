314f9b63fdaa06e6984c9c73d0351ed8437815ce
unknown <syawqiarroyan@gmail.com>
2024-10-25 15:10:47 +0700

    Part selanjutnya

--- my-app/src/pages/details.jsx
+++ my-app/src/pages/details.jsx
@@ -19,16 +19,44 @@ function Detail() {
       <div className="max-w-6xl mx-auto">
 
       </div> */}
-      <div className="max-w-6xl mx-auto flex">
+      <div className="max-w-6xl mx-auto flex justify-between">
         <img
           src={findById.poster}
           alt=""
-          className="h-80 w-80 rounded-lg shadow-lg"
+          className="h-80 w-80 rounded-lg shadow-lg object-cover"
         />
-        <div className="row ml-9">
-          <p>{findById.brand}</p>
+        <div className="row ml-9 flex-1 max-w-lg">
+          <p className="text-lg font-bold text-slate-500">{findById.brand}</p>
+          <p className="mb-7">Terjual {detailJSON.terjual}</p>
+          <p className="text-4xl font-bold font-lexend">{findById.price}</p>
+          <div className="mt-9 border-t border-b py-2">
+            <p className="font-bold font-lexend">Detail</p>
+          </div>
+          <div className="mt-4">
+            <p>
+              Kondisi:{" "}
+              <span className="text-gray-500">{detailJSON.condition}</span>
+            </p>
+            <p>
+              Etalase:{" "}
+              <span className="text-gray-500">{detailJSON.etalase}</span>
+            </p>
+            <p className="mt-5">{detailJSON.description}</p>
+          </div>
         </div>
-        <div className="cart"></div>
+        <div className="cart border  p-5">
+          <p>Atur Jumlah dan Catatan</p>
+          <div className="flex mt-9">
+            <div className="flex space-x-3 border px-3 mr-3">
+              <button>-</button>
+              <p>1</p>
+              <button>+</button>
+            </div>
+            <div className="">
+              <p>Stok Jumlah: {detailJSON.stock}</p>
+            </div>  
+          </div>
+        </div>
       </div>
     </>
   );
